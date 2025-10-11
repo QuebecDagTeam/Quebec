fba458a397d17b202e5601fb8576911d92508f89
farmerintech@gmail.com <farmerintech@gmail.com>
2025-10-11 00:58:01 -0700

    env created

--- frontend/src/components/encrypt.ts
+++ frontend/src/components/encrypt.ts
@@ -1,7 +1,7 @@
 // src/components/EncryptUtils.ts
 import CryptoJS from "crypto-js";
 
-const SECRET_KEY = 'a0f1d37ad6d239e5f6263a51380260cbec2d57d9a094a51e1c1bc2da0c2bfe35'; // set this in your .env
+const SECRET_KEY = import.meta.env.VITE_SECRET_KEY
 
 export const encryptData = (data: any) => {
   const json = JSON.stringify(data);
