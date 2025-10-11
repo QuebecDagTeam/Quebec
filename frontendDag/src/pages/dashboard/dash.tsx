7a896c6bb8d91b161b157ec4a776651eb1884770
farmerintech@gmail.com <farmerintech@gmail.com>
2025-10-11 13:01:51 -0700

    multer types

--- frontendDag/src/pages/dashboard/dash.tsx
+++ frontendDag/src/pages/dashboard/dash.tsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/logo.jpg";
 export const Dash = () => {
     return(
         <section className="bg-[#2F2F2F] flex items-center justify-between gap-[30px]">
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center text-whhite gap-4 p-4">
                 <img src={Logo} className="w-[58px] h-[58px] rounded-full"/>
                 <p className="text-[20px] font-[600]">QUEBEC</p>
             </div>
